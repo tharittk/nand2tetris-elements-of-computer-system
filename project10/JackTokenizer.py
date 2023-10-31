887f2900bf06ba5a8edf175eb232d3f295bf9fc4
Tharit Tangkijwanichakul <TharitT@pttep.com>
2023-10-31 11:33:00 +0700

    tokenizer template

---
+++ project10/JackTokenizer.py
@@ -0,0 +1,89 @@
+
+# Jack Tokenizer
+
+
+keyword = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false','null','this','let','do', 'if', 'else', 'while', 'return']
+#symbol = ['{', '}', '(', ')', '[', ']', '.', ',',';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+
+
+class Tokenizer():
+    def __init__(self, inputFile):
+        self.inputFile = inputFile
+        self.SYMBOL = ['{', '}', '(', ')', '[', ']', '.', ',',';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+        self.KEYWORD = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false','null','this','let','do', 'if', 'else', 'while', 'return']
+        self.tokens = []
+        self.currentToken = ''
+        self.charStream = ''
+        self.currentCharIdx = 0
+    def _read_input_file(self):
+        tokens = []
+        with open(self.inputFile) as f:
+            # process text file
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip()
+                # Comments or empty line
+                if line[0:2] == "/*" or line[0:2] == "\n" or line[0:2] == '//' or line == '': 
+                    pass
+                else:
+                    line = line.strip('\n')
+                    line = line.strip('\t')
+                    # inline comment
+                    line = line.split("//")[0]
+                    # Each character in line
+                    for char in line:                        
+                        self.charStream += char
+
+    def hasMoreTokens(self):
+        nchar = len(self.charStream)
+        if self.currentCharIdx < nchar - 1:
+            return True
+        else:
+            return False
+    
+    # get the next token from the input and makes it
+    # the current --- most difficult
+    def advance(self):
+        currentChar = self.charStream[self.currentCharIdx]
+        # accumulate character until becomes a token
+        
+        while (currentChar not in self.SYMBOL) or (currentChar != ' '):
+
+            self.currentToken += currentChar
+
+
+    
+    # return the type of the current token
+    def tokenType(self):
+
+        return 'KEYWORD'
+    
+    # return the keyword of the current token call iff current token type is KEYWORD
+    def keyWord(self):
+
+        return 0
+    
+    # return the symbol of the current token call iff current token type is SYMBOL
+    def symbol(self):
+
+        return 0        
+
+    # return the string of the current token call iff current token type is IDENTIFIER
+    def identifier(self):
+
+        return 0        
+    
+    # return the integer value of the current token call iff current token type is INT_CONST
+    def intVal(self):
+
+        return 0   
+
+    # return the string value of the current token call iff current token type is STRING_CONST
+    def stringVal(self):
+
+        return 0   
+
+if __name__ == "__main__":
+    tkn = Tokenizer('./Square.Jack')
+    tokens = tkn._read_input_file()
+    print(tkn.charStream)

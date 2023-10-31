cd67cfc76b10c5542909ba0893d5fd5b3a3f7f52
Tharit Tangkijwanichakul <tharitt@pttep.com>
2023-10-31 21:36:06 +0700

    pass all tokenizer test

--- project10/JackTokenizer.py
+++ project10/JackTokenizer.py
@@ -14,7 +14,7 @@ class Tokenizer():
         self.currentToken = ''
         self.charStream = ''
         self.currentCharIdx = 0
-        self.result = []
+        self.result = ['<tokens>']
     def _read_input_file(self):
         with open(self.inputFile) as f:
             # process text file
@@ -22,7 +22,7 @@ class Tokenizer():
             for line in lines:
                 line = line.strip()
                 # Comments or empty line
-                if line[0:2] == "/*" or line[0:2] == "\n" or line[0:2] == '//' or line == '': 
+                if line[0:2] == "/*" or line[0:2] == "\n" or line[0:2] == '//' or line == '' or line[0:2] == '*/' or line[0] == '*': 
                     pass
                 else:
                     line = line.strip('\n')
@@ -52,10 +52,20 @@ class Tokenizer():
         else:
             self.currentToken = ''
             # accumulate character until becomes a token
-            while (currentChar not in self.SYMBOL) and (currentChar != ' '):
+            while ((currentChar not in self.SYMBOL) and (currentChar != ' ')):
                 self.currentToken += currentChar
                 self.currentCharIdx += 1
                 currentChar = self.charStream[self.currentCharIdx]
+
+                # hit white space while in quotation
+                if self.currentToken[0] == '"' and currentChar == ' ':
+                    self.currentToken += currentChar
+                    self.currentCharIdx += 1
+                    currentChar = self.charStream[self.currentCharIdx]
+                
+                if self.currentToken[0] == '"' and self.currentToken[-1] == '"' and len(self.currentToken) >= 2:
+                    break
+
             # no appending since '' + " " shall not be accounted
             if self.currentToken != '':
                 self.tokens.append(self.currentToken)
@@ -84,7 +94,16 @@ class Tokenizer():
 
     # return the symbol of the current token call iff current token type is SYMBOL
     def symbol(self):
-        return self.currentToken
+        if self.currentToken == '<':
+            return '&lt;'
+        elif self.currentToken == '>':
+            return '&gt;'
+        elif self.currentToken == '"':
+            return '&quot;'
+        elif self.currentToken == '&':
+            return '&amp;'
+        else:
+            return self.currentToken
 
     # return the string of the current token call iff current token type is IDENTIFIER
     def identifier(self):
@@ -126,21 +145,28 @@ class Tokenizer():
                     closeBracket ='</identifier>'
 
                 elif tokenType == 'INT_CONST':
-                    openBracket = '<intConst>'
+                    openBracket = '<integerConstant>'
                     content = str(self.intVal())
-                    closeBracket ='</intConst>'
+                    closeBracket ='</integerConstant>'
 
                 elif tokenType == 'STRING_CONST':
-                    openBracket = '<stringConst>'
+                    openBracket = '<stringConstant>'
                     content = self.stringVal()
-                    closeBracket ='</stringConst>'
+                    closeBracket ='</stringConstant>'
                 
-                self.toPrint = openBracket + content + closeBracket
+                self.toPrint = openBracket +' ' + content +' '+ closeBracket
 
                 self.result.append(self.toPrint)
-
+        self.result.append('</tokens>')
 if __name__ == "__main__":
-    tkn = Tokenizer('./Square.Jack')
+    import sys
+    inputFile =sys.argv[1]
+
+    tkn = Tokenizer(inputFile)
     tkn.run()
-    for token in tkn.result:
-        print(token)
+    #for token in tkn.result:
+    #    print(token)
+    with open('./ExpressionLessSquare/Out.xml', 'w') as f:
+        for token in tkn.result:
+            f.write(token + '\n')
+        

6d6bc93696f1eef77c4fd205799070a17901dee4
Tharit Tangkijwanichakul <TharitT@pttep.com>
2023-11-01 11:16:25 +0700

    progress

--- project10/CompilationEngine.py
+++ project10/CompilationEngine.py
@@ -8,7 +8,6 @@ class CompilationEngine():
         self.currentTokenIndex = -1 # not yet look at any token
         self.result = []
         
-
     # during testing
     def _read_tokenized_xml(self, xml_file):
         xmlLines = []
@@ -16,7 +15,6 @@ class CompilationEngine():
             for line in f:
                 xmlLines.append(line.strip('\n'))
         return xmlLines
-    
 
     def _eat(self, stringIn):
         tokenAhead = self.tokenizedInputList[self.currentTokenIndex + 1]
@@ -33,10 +31,10 @@ class CompilationEngine():
     def _getCurrentTokenFull(self):
         return self.tokenizedInputList[self.currentTokenIndex]
 
-    def _getTokenContent(self):
+    def _getTokenLexical(self):
         return  self.currentToken.split(" ")[1]
     
-    def _getTokenType(self):
+    def _getTokenLexicalType(self):
         return  self.currentToken.split(">")[0].split("<")[1]
     
     def printCompileGeneral(self, stringPrint):
@@ -48,28 +46,154 @@ class CompilationEngine():
 
     # compile a complete class
     def compileClass(self):
-        pass
+
+        # 'class'
+        self._eat('class')
+        self.printCompileGeneral('<class>')
+        self.printCompiledTokenFull()
+        self._advance()
+
+        # className
+        assert self._getTokenLexicalType() == 'identifier'
+        self.printCompiledTokenFull()
+
+        # '{'
+        self._eat('{')
+        self.printCompiledTokenFull()
+        self._advance()
+
+        #
+        # class var Dec, subroutine Dec
+        #
+
+        # '}'
+        self._eat('}')
+        self.printCompiledTokenFull()
+        self.printCompileGeneral('<\class>')
 
     # compile a static variable or a field decoration
     def compileClassVarDec(self):
-        pass
+
+        self.printCompileGeneral('<classVarDec>')
+        # (static | field)
+        self._advance()
+        assert self._getTokenLexical() in ['static', 'field']
+        self.printCompiledTokenFull()
+        self._advance()
+
+        # type
+        assert (self._getTokenLexical() in ['int', 'char', 'boolean']) or self._getTokenLexicalType() == 'identifier'
+        self.printCompiledTokenFull()
+        self._advance()
+
+        # varName
+        assert self._getTokenLexicalType() == 'identifier'
+        self.printCompiledTokenFull()
+        self._advance()
+
+        #
+        # optional other ',' + varName*
+        #
+
+        # ';'
+        self._eat(';')
+        self.printCompiledTokenFull()
+        self.printCompileGeneral('<\classVarDec>')
 
     # compile a complete method, function, or constructor
     def compileSubroutine(self):
-        pass
+
+        self.printCompileGeneral('<subroutineDec>')
+
+        # (constructor | function | method)
+        assert self._getTokenLexical() in ['constructor', 'function', 'method']
+        self.printCompiledTokenFull()
+        self._advance()
+
+        # ('void' | type)
+        assert (self._getTokenLexical() in ['void','int', 'char', 'boolean']) or self._getTokenLexicalType() == 'identifier' 
+        self.printCompiledTokenFull()
+        self._advance()
+
+        # subRoutineName
+        assert self._getTokenLexicalType() == 'identifier'
+        self.printCompiledTokenFull()
+        self._advance()
+
+        # '('
+        self._eat('(')
+        self.printCompiledTokenFull()
+
+        #
+        # ParameterList
+        #
+
+        # '('
+        self._eat(')')
+        self.printCompiledTokenFull()
+
+        #
+        # SubRoutineBody
+        #
+
+        self.printCompileGeneral('<\subroutineDec>')
+
 
     # compile a (possibly empty) parameter list
     # does not handle the enclosing parentheses tokens ( and )
     def compileParameterList(self):
-        pass
+        self.printCompileGeneral('<parameterList>')
+
+        # optional - all
+
+        # type
+        #assert (self._getTokenLexical() in ['int', 'char', 'boolean']) or self._getTokenLexicalType() == 'identifier'
+        #self.printCompiledTokenFull()
+        #self._advance()
+
+        # varName
+        #assert self._getTokenLexicalType() == 'identifier'
+        #self.printCompiledTokenFull()
+        #self._advance()
+
+        self.printCompileGeneral('<\parameterList>')
 
     # compile a subroutine's body
     def compileSubroutineBody(self):
-        pass
+        self.printCompileGeneral('<subRoutineBody>')
+        self._eat('{')
+        self.printCompiledTokenFull()
+
+        #
+        # statements
+        #
+
+        self._eat('}')
+        self.printCompiledTokenFull()
+        self.printCompileGeneral('</subRoutineBody>')
 
     # compile a var declaration
     def compileVarDec(self):
-        pass
+        self.printCompileGeneral('<varDec>')
+        # 'var'
+        self._eat('var')
+        self.printCompiledTokenFull()
+
+        # type
+        assert (self._getTokenLexical() in ['int', 'char', 'boolean']) or self._getTokenLexicalType() == 'identifier'
+        self.printCompiledTokenFull()
+        self._advance()
+
+        # varName
+        assert self._getTokenLexicalType() == 'identifier'
+        self.printCompiledTokenFull()
+        self._advance()
+
+        # optional ------
+        # while next is ','
+
+
+        self.printCompileGeneral('</varDec>')
 
     # compile a sequents of statements
     # does not handle enclosing { }
@@ -78,27 +202,81 @@ class CompilationEngine():
 
     # compile a let statement
     def compileLet(self):
-        pass
+        self._eat('let')
+        self.printCompileGeneral('<letStatement>')
+        self.printCompiledTokenFull()
+
+        # compileterm
+        
+        self._eat('=')
+        self.printCompiledTokenFull()
+        
+        # compile expression
+
+        self.printCompileGeneral('</letStatement>')
 
     # compile an if with possibly else clause
     def compileIf(self):
-        pass
+        self._eat('if')
+        self.printCompileGeneral('<ifStatement>')
+        self.printCompiledTokenFull()
+
+        self._eat('(')
+        self.printCompiledTokenFull()
+
+        # expression
+
+        self._eat(')')
+        self.printCompiledTokenFull()
+
+
+        self._eat('{')
+        self.printCompiledTokenFull()
+
+        # statements
+
+        self._eat('}')
+        self.printCompiledTokenFull()
+
+        #optional else - no need explicit handling ?
+      
+        try:
+            self._eat('else')
+            self.printCompiledTokenFull()
+
+            self._eat('{')
+            self.printCompiledTokenFull()
+
+            # statements
+
+            self._eat('}')
+            self.printCompiledTokenFull()
+        
+        except:
+
+            pass
+
+        self.printCompileGeneral('</ifStatement>')
 
     # compile a while statement
     def compileWhile(self):
-    #    eat(while); code to handle / write while
-    #    eat ('('); code to hand
-    #    compileExpression
         self._eat('while')
         self.printCompileGeneral('<whileStatement>')
         self.printCompiledTokenFull()
+
         self._eat('(')
         self.printCompiledTokenFull()
-        # recursion
+
+        # expression
+
         self._eat(')')
         self.printCompiledTokenFull()
+
         self._eat('{')
         self.printCompiledTokenFull()
+
+        # statements
+
         self._eat('}')
         self.printCompiledTokenFull()
         self.printCompileGeneral('</whileStatement>')
@@ -106,15 +284,32 @@ class CompilationEngine():
 
     # compile a do statement
     def compileDo(self):
-        pass
+        self._eat('do')
+        self.printCompileGeneral('<doStatement>')
+
+        # subroutine call
+
+        self.printCompileGeneral('</doStatement>')
 
     # compile a return statement
     def compileReturn(self):
-        pass
+        self._eat('return')
+        self.printCompileGeneral('<returnStatement>')
+
+        # optional expression
+
+        self._eat(';')
+        self.printCompiledTokenFull()
+        self.printCompileGeneral('</returnStatement>')
 
     # compile an expression
     def compileExpression(self):
-        pass
+        
+        # PLACEHOLDER
+        self._advance()
+        self.printCompiledTokenFull()
+        #
+
 
     # compile a term
     # if current token is identifier,
@@ -136,14 +331,8 @@ class CompilationEngine():
 
 if __name__ == "__main__":
 
-    #test = ['<tokens>','<keyword> while <\keyword>', '<symbol> ( <\symbol>','<\tokens>']
     cpe = CompilationEngine('./OutWhile.xml')
-    
     print(cpe.tokenizedInputList)
-
     cpe.run()
-    #cpe._eat('while')
-    #print(cpe.currentToken, cpe.currentTokenIndex)
-    #print(cpe.getTokenContent(), cpe.getTokenType())
     for line in cpe.result:
         print(line)

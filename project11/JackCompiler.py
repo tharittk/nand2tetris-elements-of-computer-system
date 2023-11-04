c27d019cf0ceb81000a8796af82851b83b03fde4
Tharit Tangkijwanichakul <tharitt@pttep.com>
2023-11-04 21:30:57 +0700

    vmwriter

---
+++ project11/JackCompiler.py
@@ -0,0 +1,36 @@
+from JackTokenizer import Tokenizer
+from CompilationEngine import CompilationEngine
+
+import sys, os
+if __name__ == '__main__':
+
+    inputName =sys.argv[1]
+
+    #folderName = os.path.dirname(inputFile)
+    #fileName = os.path.basename(inputFile)
+    
+    # single file
+    if not os.path.isdir(inputName):
+        pass
+
+    # a folder
+    else:
+
+
+        for fileName in os.listdir(inputName):
+
+            if fileName.endswith(".jack"):
+
+                outTokenizedFile = inputName + '/' + fileName[:-5] + 'T' + '.xml'
+                outVMFile = inputName + '/' + fileName[:-5]  + '.vm'
+
+                # output xml file - tokenized
+                tkn = Tokenizer(inputName + '/' + fileName)
+                tkn.run()
+                tkn.outputting(outTokenizedFile)
+
+                # output vm file
+
+
+
+
---
+++ project11/VMWriter.py
@@ -0,0 +1,78 @@
+# VMWriter
+
+
+class VMWriter():
+
+    def __init__(self, outputFileName):
+        self.outputName = outputFileName
+
+    def _open_and_write(self, toWrite):
+        with open(self.outputName, 'w') as f:
+            f.write(toWrite + '\n')      
+
+    # write VM push command
+    def writePush(self, segment, idx):
+        toWrite = '''
+                    push {segment} {idx}
+                '''.format(segment = segment.lower(), idx = idx)
+
+        self._open_and_write(toWrite)
+
+    # write VM pop command
+    def writePop(self, segment, idx):
+        toWrite = '''
+                    pop {segment} {idx}
+                '''.format(segment = segment.lower(), idx = idx)
+
+        self._open_and_write(toWrite)
+
+    # write VM arithmatic command
+    def writeArithmatic(self, command):
+
+        self._open_and_write(command.lower())
+
+            
+    # write VM label command
+    def writeLabel(self, labelName):
+        toWrite = '''
+                    label {labelName}
+                '''.format(labelName = labelName)
+
+        self._open_and_write(toWrite)
+
+    # write VM go-to command
+    def writeGoto(self, labelName):
+        toWrite = '''
+                    goto {labelName}
+                '''.format(labelName = labelName)
+
+        self._open_and_write(toWrite)
+
+    # write VM if-goto command
+    def writeIf(self, labelName):
+        toWrite = '''
+                    if-goto {labelName}
+                '''.format(labelName = labelName)
+
+        self._open_and_write(toWrite)
+
+
+    # write VM call command
+    def writeCall(self, callName, nArgs):
+        toWrite = '''
+                    call {callName} {nArgs}
+                '''.format(callName = callName, nArgs = str(nArgs))
+
+        self._open_and_write(toWrite)
+
+
+    # write VM Function command
+    def writeFunction(self, funcName, nVars):
+        toWrite = '''
+                    function {funcName} {nVars}
+                '''.format(funcName = funcName, nVars = str(nVars))
+        self._open_and_write(toWrite)
+
+    def writeReturn(self):
+        self._open_and_write('return')
+     
